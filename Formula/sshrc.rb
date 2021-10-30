d611748c67cc2f51febc90c6cae1cf43f0acb307
Ikuo Degawa <ikuwow@users.noreply.github.com>
2021-10-30 12:39:26 +0900

    Update sshrc.rb
    #1

--- Formula/sshrc.rb
+++ Formula/sshrc.rb
@@ -5,8 +5,6 @@ class Sshrc < Formula
   sha256 "09dc3ab22d07674fd7f226de5b16689012d30bda1e95a84354c0902cf10991f8"
   head "https://github.com/ikuwow/sshrc.git"
 
-  bottle :unneeded
-
   def install
     bin.install %w[sshrc moshrc]
   end
